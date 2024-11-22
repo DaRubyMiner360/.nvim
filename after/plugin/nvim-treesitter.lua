deabbd877d8e803518ffbfc9965c50dc050d26c1
DevReaper0 <devreaper0@gmail.com>
2024-11-22 04:40:35 +0000

    Fixed an error in the previous commit.

--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -10,29 +10,28 @@ vim.filetype.add({
 vim.api.nvim_create_autocmd('BufRead', {
   pattern = { 'Caddyfile', '*.caddyfile' },
   callback = function()
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.caddyfile = {
+    require("nvim-treesitter.parsers").get_parser_configs().caddyfile = {
       install_info = {
         url = "https://github.com/matthewpi/tree-sitter-caddyfile", -- local path or git repo
-        files = { "src/parser.c" },                             -- note that some parsers also require src/scanner.c or src/scanner.cc
+        files = { "src/parser.c" },                                 -- note that some parsers also require src/scanner.c or src/scanner.cc
         -- optional entries:
-        generate_requires_npm = false,                          -- if stand-alone parser without npm dependencies
-        requires_generate_from_grammar = false,                 -- if folder contains pre-generated src/parser.c
+        generate_requires_npm = false,                              -- if stand-alone parser without npm dependencies
+        requires_generate_from_grammar = false,                     -- if folder contains pre-generated src/parser.c
       },
-      filetype = "caddyfile",                                   -- if filetype does not match the parser name
+      filetype = "caddyfile",                                       -- if filetype does not match the parser name
     }
-  end,
-})
 
-require("nvim-treesitter.configs").setup({
-  highlight = {
-    enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+    require("nvim-treesitter.configs").setup({
+      highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
 })
 
 vim.cmd('TSUpdate')
